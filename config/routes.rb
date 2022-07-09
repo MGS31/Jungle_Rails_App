2856d802ff8d4840f7ae004d0c412b25743264c5
Matt <mgstankov30@gmail.com>
2022-07-09 15:11:33 -0400

    update routes to ensure the proper routes are in place for the categories page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'categories/index'
+    get 'categories/show'
+  end
   get 'about/index'
   root to: 'products#index'
 
@@ -16,6 +20,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
